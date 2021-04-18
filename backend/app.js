43709ee8e7fd618f1d75c4107f8ebd0c9aba0abf
Kiran Jagathlal <kiranjagz@gmail.com>
2021-04-18 18:47:27 +0200

    Format changes

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,9 @@ mongoose
     });
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use("/images", express.static(path.join("backend/images")));
 
 app.use((req, res, next) => {
