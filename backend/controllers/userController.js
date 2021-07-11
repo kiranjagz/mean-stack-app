77435025f2265169fc7d623bdc3a82e796563ea8
Kiran Jagathlal <kiranjagz@gmail.com>
2021-07-11 15:57:52 +0200

    Added middleware for controllers

---
+++ backend/controllers/userController.js
@@ -0,0 +1,67 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+exports.createUser = (req, res, next) => {
+    bcrypt.hash(req.body.password, 10).then(hash => {
+
+        const user = new User({
+            email: req.body.email,
+            password: hash
+        });
+
+        user.save()
+            .then(result => {
+                res.status(201).json({
+                    message: "User Created!",
+                    result: result
+                });
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: "Invalid authenication credentials!"
+                });
+            })
+    });
+}
+
+exports.loginUser = (req, res, next) => {
+    let fetchedUser;
+    User.findOne({
+            email: req.body.email
+        })
+        .then(user => {
+            if (!user) {
+                return res.status(401).json({
+                    message: "Cannot find user for that email address: " + req.body.email
+                });
+            }
+            fetchedUser = user;
+            return bcrypt.compare(req.body.password, user.password)
+        })
+        .then(result => {
+            if (!result) {
+                return res.status(401).json({
+                    message: "Crypt compare failed, please contact Bob :)"
+                });
+            }
+
+            const token = jwt.sign({
+                email: fetchedUser.email,
+                userId: fetchedUser._id
+            }, "secret_bob", {
+                expiresIn: "1h"
+            });
+            res.status(200).json({
+                token: token,
+                expiresIn: 3600,
+                userId: fetchedUser._id
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            return res.status(401).json({
+                message: "Invalid login authenication credentials!"
+            });
+        });
+}
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,72 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+const UserController = require("../controllers/userController");
 
-const User = require("../models/user");
+router.post("/signup", UserController.createUser);
 
-router.post("/signup", (req, res, next) => {
-    bcrypt.hash(req.body.password, 10).then(hash => {
-
-        const user = new User({
-            email: req.body.email,
-            password: hash
-        });
-
-        user.save()
-            .then(result => {
-                res.status(201).json({
-                    message: "User Created!",
-                    result: result
-                });
-            })
-            .catch(err => {
-                res.status(500).json({
-                    message: "Invalid authenication credentials!"
-                });
-            })
-    });
-});
-
-router.post("/login", (req, res, next) => {
-    let fetchedUser;
-    User.findOne({
-            email: req.body.email
-        })
-        .then(user => {
-            if (!user) {
-                return res.status(401).json({
-                    message: "Cannot find user for that email address: " + req.body.email
-                });
-            }
-            fetchedUser = user;
-            return bcrypt.compare(req.body.password, user.password)
-        })
-        .then(result => {
-            if (!result) {
-                return res.status(401).json({
-                    message: "Crypt compare failed, please contact Bob :)"
-                });
-            }
-
-            const token = jwt.sign({
-                email: fetchedUser.email,
-                userId: fetchedUser._id
-            }, "secret_bob", {
-                expiresIn: "1h"
-            });
-            res.status(200).json({
-                token: token,
-                expiresIn: 3600,
-                userId: fetchedUser._id
-            });
-        })
-        .catch(err => {
-            console.log(err);
-            return res.status(401).json({
-                message: "Invalid login authenication credentials!"
-            });
-        });
-});
+router.post("/login", UserController.loginUser);
 
 module.exports = router;
